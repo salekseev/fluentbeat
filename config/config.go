fbd987762b8d7afa3c86a52da550551ad648d88e
Stas Alekseev <stas.alekseev@gmail.com>
2017-03-02 14:59:50 -0500

    Adding initial config

--- config/config.go
+++ config/config.go
@@ -6,9 +6,19 @@ package config
 import "time"
 
 type Config struct {
-	Period time.Duration `config:"period"`
+	Period time.Duration			  time.Duration     `config:"period"`
+	Port                 			  int               `config:"port"`
+	Addr                 			  string
+	EnableJsonValidation              bool              `config:"enable_json_validation"`
+	PublishFailedJsonSchemaValidation bool              `config:"publish_failed_json_schema_validation"`
+	PublishFailedJsonInvalid          bool              `config:"publish_failed_json_invalid"`
+	JsonDocumentTypeSchema            map[string]string `config:"json_document_type_schema"`
 }
 
 var DefaultConfig = Config{
-	Period: 1 * time.Second,
+	Period:							   1 * time.Second,
+	Port:							   24224,
+	EnableJsonValidation:              false,
+	PublishFailedJsonSchemaValidation: false,
+	PublishFailedJsonInvalid:          false,
 }
